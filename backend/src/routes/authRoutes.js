687d1c8d6fd55b5ed52c6667f97b067c081c53de
Thisaru Rajapaksha <162295795+thisaru01@users.noreply.github.com>
2026-02-27 17:57:39 +0530

    feat: add user registration validation

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import * as authController from "../controllers/authController.js";
 import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
+import { validateUserRegister } from "../validations/userValidation.js";
 
 const router = express.Router();
 
-router.post("/register", authController.register);
+router.post("/register", validateUserRegister, authController.register);
 router.post("/login", authController.login);
 
 // Example protected route for testing
