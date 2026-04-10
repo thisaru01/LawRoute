aa94921dc92818e84895cd2b088a2c1574f575a6
Inushi Kavindi <inushikavindi@gmail.com>
2026-04-10 16:06:46 +0530

    refactor: move document validation logic to validation folder

--- backend/src/services/documents/documentService.js
+++ backend/src/services/documents/documentService.js
@@ -1,31 +1,16 @@
 import mongoose from "mongoose";
 import { cloudinary } from "../../config/cloudinary.js";
 import Document from "../../models/documents/documentModel.js";
+import {
+  ensureValidDocumentId,
+  ensureUserAuthenticated,
+  validateCreatePayload,
+  validateUpdatePayload,
+  validateDeletePayload,
+} from "../../validations/documents/documentValidation.js";
 
 export const createDocument = async ({ title, description, user, fileUrl, filePublicId, fileType }) => {
-  if (!user || !user._id) {
-    const err = new Error("Unauthorized");
-    err.status = 401;
-    throw err;
-  }
-
-  if (user.role !== "admin") {
-    const err = new Error("Only admins can upload documents");
-    err.status = 403;
-    throw err;
-  }
-
-  if (!title) {
-    const err = new Error("Title is required");
-    err.status = 400;
-    throw err;
-  }
-
-  if (!fileUrl || !filePublicId) {
-    const err = new Error("PDF file is required");
-    err.status = 400;
-    throw err;
-  }
+  validateCreatePayload({ user, title, fileUrl, filePublicId });
 
   let thumbnailUrl;
 
@@ -62,13 +47,7 @@ export const getAllDocuments = async () => {
 };
 
 export const getDocumentById = async ({ id }) => {
-  const cleanId = String(id).replace(/[<>]/g, "");
-
-  if (!mongoose.Types.ObjectId.isValid(cleanId)) {
-    const err = new Error("Invalid document id");
-    err.status = 400;
-    throw err;
-  }
+  const cleanId = ensureValidDocumentId(id);
 
   const doc = await Document.findById(cleanId);
   if (!doc) {
@@ -81,25 +60,8 @@ export const getDocumentById = async ({ id }) => {
 };
 
 export const updateDocument = async ({ id, title, description, user }) => {
-  const cleanId = String(id).replace(/[<>]/g, "");
-
-  if (!mongoose.Types.ObjectId.isValid(cleanId)) {
-    const err = new Error("Invalid document id");
-    err.status = 400;
-    throw err;
-  }
-
-  if (!user || !user._id) {
-    const err = new Error("Unauthorized");
-    err.status = 401;
-    throw err;
-  }
-
-  if (user.role !== "admin") {
-    const err = new Error("Only admins can update documents");
-    err.status = 403;
-    throw err;
-  }
+  const cleanId = ensureValidDocumentId(id);
+  validateUpdatePayload({ user });
 
   const doc = await Document.findById(cleanId);
   if (!doc) {
@@ -115,25 +77,8 @@ export const updateDocument = async ({ id, title, description, user }) => {
   return doc;
 };
 export const deleteDocument = async ({ id, user }) => {
-  const cleanId = String(id).replace(/[<>]/g, "");
-
-  if (!mongoose.Types.ObjectId.isValid(cleanId)) {
-    const err = new Error("Invalid document id");
-    err.status = 400;
-    throw err;
-  }
-
-  if (!user || !user._id) {
-    const err = new Error("Unauthorized");
-    err.status = 401;
-    throw err;
-  }
-
-  if (user.role !== "admin") {
-    const err = new Error("Only admins can delete documents");
-    err.status = 403;
-    throw err;
-  }
+  const cleanId = ensureValidDocumentId(id);
+  validateDeletePayload({ user });
 
   const doc = await Document.findById(cleanId);
   if (!doc) {
